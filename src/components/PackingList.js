116a4783bbb70cb22b599c9bf320ec64774b970b
M00nNight <khanzamil267@gmail.com>
2023-06-21 11:50:00 +0530

    my first commit

---
+++ src/components/PackingList.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function PackingList({
+  items,
+  onDeleteItems,
+  onToggleItem,
+  setItems,
+}) {
+  const [sortby, setSortby] = useState("input");
+  function clearList() {
+    const confirmed = window.confirm("Are you sure want to delete all items?");
+    if (confirmed) setItems([]);
+  }
+  let sortedItems;
+  if (sortby === "input") sortedItems = items;
+  if (sortby === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortby === "packed")
+    sortedItems = items.slice().sort((a, b) => Number(b.packed));
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItems={onDeleteItems}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortby} onChange={(e) => setSortby(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={() => clearList(items.id)}>Clear list</button>
+      </div>
+    </div>
+  );
+}
+function Item({ item, onDeleteItems, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
+    </li>
+  );
+}
